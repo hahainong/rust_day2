2bbb11975ae60a02730d7b37840379651a63afcf
haiha04 <hanong0410@gmail.com>
2022-10-24 13:02:23 +0700

    Inittial commit rust example day 2

---
+++ target/debug/deps/rust_day2-18ad6d30d97b720a.d
@@ -0,0 +1,5 @@
+/Users/nonghaiha/Workspace/Caerux/Projects/blockchain/homework/rust_day2/target/debug/deps/rust_day2-18ad6d30d97b720a: src/main.rs
+
+/Users/nonghaiha/Workspace/Caerux/Projects/blockchain/homework/rust_day2/target/debug/deps/rust_day2-18ad6d30d97b720a.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust_day2.d
@@ -0,0 +1,1 @@
+/Users/nonghaiha/Workspace/Caerux/Projects/blockchain/homework/rust_day2/target/debug/rust_day2: /Users/nonghaiha/Workspace/Caerux/Projects/blockchain/homework/rust_day2/src/main.rs
